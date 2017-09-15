72a7bab04cd901d9c4bbaee2d3f04318ab870011
gabrielripardo <gts.senna@gmail.com>
2017-09-15 03:21:23 -0300

    Agenda em terminal

---
+++ executavel.sh
@@ -0,0 +1,19 @@
+echo ''
+echo "                                Agenda_Console"
+echo ''
+echo ''
+echo "                                  versão 1.0"
+echo "                              por Gabriel Ripardo"
+echo " A Agenda_Console é um simples open-source feito em java para fins de aprendizado"
+echo ''
+
+#[1] Entrando no diretório
+cd src
+
+#[2] Compilando a classe com o método principal
+javac Start.java
+
+#[3] Rodando o programa junto com driver do SQLite
+java -cp sqlite-jdbc-3.20.0.jar: Start
+
+
